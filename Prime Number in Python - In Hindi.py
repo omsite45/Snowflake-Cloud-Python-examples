179e5f53194e10ea78c1b94408773f69a2eeb8e1
omsite45 <116798652+omsite45@users.noreply.github.com>
2022-10-27 16:13:14 +0530

    Add files via upload

---
+++ Prime Number in Python - In Hindi.py
@@ -0,0 +1,22 @@
+
+
+
+n= int(input("Enter the number "))
+
+Count=0
+if(n>0):
+    for i in range(1,n+1):
+        if(n%i==0):
+            Count+=1
+    if(Count==2):
+        print("primt Number")
+    else:
+        print("Composite Number")
+else:
+    print("Enter greater than zero")
+    
+        
+    
+
+        
+            
