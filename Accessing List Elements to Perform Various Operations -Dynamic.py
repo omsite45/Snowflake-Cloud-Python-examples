1fd3ed203863cae9d10a47696eb5a4d24d7be3d2
omsite45 <116798652+omsite45@users.noreply.github.com>
2022-10-29 19:51:40 +0530

    Add files via upload

--- Accessing List Elements to Perform Various Operations -Dynamic.py
+++ Accessing List Elements to Perform Various Operations -Dynamic.py
@@ -1,11 +1,21 @@
+import ast
 def dynamic_input_list():
-    lst = []
-    n=int(input("Enter the number of elements you wanna to enter in the list "))
+    lst1 = []
+    lst2 = []
+    n=int(input("Enter the number of elements you wanna to enter in the list 1 "))
+    n1=int(input("Enter the number of elements you wanna to enter in the list 2 "))
+    s=1
     for i in range(1,n+1):
-        print("Enter the ",i,"element ", end="")
-        ele=input()
-        lst.append(ele)
-        n-=1
-    print (lst)
-
+            print("Enter the ",i,"element ", end="")
+            ele=str(input())
+            lst1.append(ele)
+    print (lst1)
+    for i in range(1,n1+1):
+            print("Enter the ",i,"element ", end="")
+            ele=str(input())
+            lst2.append(ele)
+    print (lst2)
+    
+    
+    
 dynamic_input_list()
