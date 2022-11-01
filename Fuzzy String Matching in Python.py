fb349845632231750d7dfa841ca8232d3aed5eff
omsite45 <116798652+omsite45@users.noreply.github.com>
2022-11-01 13:28:08 +0530

    Add files via upload

---
+++ Fuzzy String Matching in Python.py
@@ -0,0 +1,5 @@
+from thefuzz import fuzz, process
+s1=input("Enter the input1 ")
+s2=input("Enter the input1 ")
+print(fuzz.partial_ratio(s1,s2))
+print("Token Sort Ratio ", fuzz.token_sort_ratio(s1,s2))
