3a54c020c0790798453cc34433fe918376a49c23
omsite45 <116798652+omsite45@users.noreply.github.com>
2022-12-15 21:30:24 +0530

    Add files via upload
    MIGRATE DATA FROM MS SQL TO SNOWFLAKE CLOUD USING PYTHON

---
+++ Python on Snowflake 1 Million Record Migration from SQL Server using read_sql and write_pandas.py
@@ -0,0 +1,54 @@
+import snowflake.connector as sn
+from snowflake.connector.pandas_tools import write_pandas
+import pyodbc as pyo
+import sys
+import os
+import numpy as np
+import pandas as pd
+
+print('opening sql server.. ')
+cnn_sql=pyo.connect(
+    r"Driver={SQL Server};Server=DESKTOP-1OACCVP\SQLEXPRESS;"
+    "Database=AdventureWorksLT2019; UID=sa1;PWD=sa;"
+    )
+
+cursor = cnn_sql.cursor()
+mylist_rep1 = []
+print('opened..')
+sql="select * from  AdventureWorksLT2019.[SalesLT].[Address];"
+df=pd.read_sql(sql, cnn_sql)
+rep=df.to_string()
+print(rep.replace("\\n", " "))
+
+
+
+
+print('SQL closed')
+
+
+scnn=sn.connect(user='swamilaxmi4',
+    password='Omsite@405',
+    account='buabvey-qf15468',
+    warehouse='SAMPLE',
+    database='AdventureWorksLT2022',
+    schema='Public'
+    )
+cur=scnn.cursor()
+cur.execute("use warehouse SAMPLE_WH_SYS_ADMIN")
+cur.execute("Use  AdventureWorksLT2022")
+cur.execute("CREATE  TABLE  Address (AddressID int IDENTITY(1,1)  NOT NULL,AddressLine1 string NOT NULL,AddressLine2 STRING,City STRING NOT NULL,StateProvince STRING NOT NULL,CountryRegion STRING NOT NULL,PostalCode STRING NOT NULL,rowguid STRING   NOT NULL,ModifiedDate datetime NOT NULL);")
+print('insert into snowflake table')
+cs=scnn.cursor()
+
+print("Writing to snowflake table ...")
+
+success, num_chunks, num_rows, output = write_pandas(
+            scnn,
+            df ,
+            "Address",
+            quote_identifiers=False
+         )
+scnn.commit()
+scnn.close()
+print("Closed..")
+
